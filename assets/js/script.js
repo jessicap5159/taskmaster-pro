39a99674ce0116923fcde74cde574f0af31c31ce
jessicap5159 <jessicaj5159@gmail.com>
2021-02-26 10:49:07 -0800

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,16 +61,20 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
+    $(this).addClass("dropover"); 
+    $(".bottom-trash").addClass("bottom-trash-drag"); 
     console.log("activate", this);
   },
   deactivate: function (event) {
+    $(this).removeClass("dropover"); 
+    $(".bottom-trash").removeClass("bottom-trash-drag");
     console.log("deactivate", this);
   },
   over: function (event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function (event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active"); 
   },
   update: function() {
     var tempArr = [];
@@ -109,12 +113,15 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
     console.log("drop");
   },
   over: function(event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
     console.log("over");
   },
   out: function(event,ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
     console.log("out");
   }
  });
@@ -238,7 +245,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -295,3 +302,10 @@ $("#modalDueDate").datepicker({
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function () {
+$(".card .list-group-item").each(function(index, el) {
+  auditTask(el)
+  console.log(taskEl);
+});
+}, (1000 * 60) * 30 ); 
